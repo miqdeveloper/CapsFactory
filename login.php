1304438751167559fa694bba0553aabe560e70d2
miqueias <miqueiasemaira@gmail.com>
2020-07-03 14:55:44 -0300

    teste mongo 14:55

--- login.php
+++ login.php
@@ -1,1 +1,6 @@
-<?php echo "Hello Wolrd";?>
+<?php
+    $client = new MongoDB\Client(
+        'mongodb+srv://mongoadmin:<password>@cluster0.qeq7i.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority');
+        $db = $client->test; 
+        echo '<h1>'. $db.'<h1>';
+?>
