83b1bec4ebc0253aa3741748567ded47f03dc9c6
Miqueias <miqueiasemaira@gmail.com>
2020-07-08 13:10:35 -0300

    add file index.php

--- login.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $client = new MongoDB\Client(
-        'mongodb+srv://mongoadmin:<password>@cluster0.qeq7i.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority');
-        $db = $client->test; 
-        echo '<h1>'. $db.'<h1>';
-?>
