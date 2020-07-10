98943c6f5eef0eae68b3ff7c473ee1d7829f48cf
Miqueias <miqueiasemaira@gmail.com>
2020-07-10 19:57:24 -0300

    mysql

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
- header('Location: https://capsfactory.herokuapp.com/login.html')
-?>
+ <?php
+ 
+ 
+ $host="localhost";
+ $port=3306;
+ $socket="";
+ $user="root";
+ $password="root";
+ $dbname="login";
+ 
+ 
+   $con = mysqli_connect ($host, $user, $password, $dbname);
+ 
+   if (!$con){
+     echo "<script>window.alert('ERROR') window.location.href='$index'</script>";
+   }
+   
+   
+ ?>
