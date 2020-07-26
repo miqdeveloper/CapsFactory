132a6f9951be8cd8681f61b5564e595bdd3edad1
Miqueias <miqueiasemaira@gmail.com>
2020-07-26 12:39:57 -0300

    TEST verifica.php

--- links.php
+++ links.php
@@ -1,6 +1,5 @@
 <?php
   $host = 'https://capsfactory.herokuapp.com';
-  $index = $host.'/index.php';
   $login =  $host.'/index.php';
   $insc =  $host.'/cad.php';
   $dash_user = $host.'/DashboardUser.php'; 
